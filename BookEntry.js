30763849754ef9539e3b7c894bed3c1a3ff1e01a
nicholasjdc <nicholasdentoncheng@gmail.com>
2023-11-04 15:51:02 -0400

    init structure

---
+++ BookEntry.js
@@ -0,0 +1,15 @@
+class BookEntry {
+    constructor(entryNumber,author, title, publication, pageCount, ISBN, seriesTitle, note, resource, languageCode){
+        this.entryNumber = entryNumber;
+        this.author = author;
+        this.title = title;
+        this.publication = publication;
+        this.pageCount = pageCount;
+        this.ISBN = ISBN;
+        this.seriesTitle = seriesTitle;
+        this.note = note;
+        this.resource = resource;
+        this.languageCode = languageCode;
+
+    }
+}
